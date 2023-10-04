0985e1394efb6cfd2fa8d88e3eea57ae34862512
Ziyi Chen <zc2913@nyu.edu>
2023-10-04 15:47:04 -0400

    array

--- data-structures/sketch.js
+++ data-structures/sketch.js
@@ -1,42 +1,33 @@
-let xPos = [0,  0, 0];
-let yPos = [150,250,350];
-let xVel = [2,3,4];
-let cDiam = [40, 40, 40];
+let xPos = [];
+let yPos = [];
+let xVel = [];
+let cDiam = [];
+let numCircles = 10;
 
 
 function setup() {
   createCanvas(800, 600);
-}
+  for(let i = 0; i < numCircles; i ++){
+     xPos.push(0);
+     yPos.push(random(30, height - 30));
+     xVel.push(random(2,5));
+     cDiam.push(random(20, 70));
+}}
 
 function draw() {
   background("lightblue");
-  fill(20, 0,random(100,255));
+  fill(20, 10,100);
 
-  // draw circle
-  ellipse(xPos[0], yPos[0], cDiam[0], cDiam[0]);
-  ellipse(xPos[1], yPos[1], cDiam[1], cDiam[1]);
-  ellipse(xPos[2], yPos[2], cDiam[2], cDiam[2]);
-
-  // update pos
-  xPos[0] = xPos[0]+ xVel[0];
-  xPos[1] = xPos[1] + xVel[1];
-  xPos[2] = xPos[2] + xVel[2];
+  for(let i = 0; i < numCircles; i +=1){
+    ellipse(xPos[i],yPos[i],cDiam[i],cDiam[i])
+  xPos[i] = xPos[i] + cDiam[i];
 
   // if reset is needed: pick random diameter and reset x
-  if (xPos[0] > width + cDiam[0]) {
-    cDiam[0] = random(20, 75);
-    xPos[0 ]= 0 - cDiam[0];
+  if (xPos[i] > width + cDiam[i]) {
+    cDiam[i] = random(20, 75);
+    xPos[i]= 0 - cDiam[i];
   }
-  if (xPos[1] > width + cDiam[1]) {
-    cDiam[1] = random(50, 125);
-    xPos[1 ]= 0 - cDiam[1];
   }
-  if (xPos[2] > width + cDiam[2]) {
-    cDiam[2] = random(75, 200);
-    xPos[2 ]= 0 - cDiam[2];
   }
-  
 
-
-  }
 
